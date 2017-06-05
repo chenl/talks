e1babe8df16ec5493c6e70b8832fecbd4b1cbf30
Chen Rotem Levy <chen@rotemlevy.name>
2017-06-05 21:33:16 +0300

    T now uses google cloud translate
    also i18n has lang and not is_lang

--- 20170613-mock/code/guessing-ee/guessing/i18n.py
+++ 20170613-mock/code/guessing-ee/guessing/i18n.py
@@ -1,27 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from os import environ
+from itertools import chain
+from google.cloud import translate
 
-def is_lang(lang):
-    return any(environ.get(key, 'C').startswith(lang)
-               for key in ('LC_MESSAGES', 'LC_ALL', 'LANG'))
+CLIENT = translate.Client()
 
-def T(text):
-    if is_lang('he'):
-        return text[::-1].upper()
-    else:
-        return text
+def lang():
+    # type: () -> str
+    lang_keys = ('LC_MESSAGES', 'LANG', 'LC_ALL')
+    lang_env = (environ.get(key) for key in lang_keys)
+    return next(filter(None, chain(lang_env, 'C')))[:2]
 
 def is_quit(text):
     # type: (str) -> bool
-    if is_lang('he'):
+    if lang() == 'he':
         return text.strip() in ['לא', 'די', 'מספיק', 'צא בחוץ']
     else:
         return text.strip().lower() in ['quit', 'stop', 'exit', 'no', 'enough']
 
 def is_yes(text):
     # type: (str) -> bool
-    if is_lang('he'):
+    if lang == 'he':
         return text.strip() in ['כ', 'כן', 'בטח', 'סבבה', '']
     else:
         return text.strip().lower() in ['', 'y', 'yes', 'yep', 'sure']
+
+def T(text):
+    # type: (str) -> str
+    lng = lang()
+    if lng in ('C', 'en'):
+        return text
+    tr = CLIENT.translate(
+        values=text,
+        format_='text',
+        target_language=lng,
+        source_language='en')
+    return tr[0]['translatedText']
