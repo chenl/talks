9fa222a1a03f414090737e975a72863c7b7a6c64
Chen Rotem Levy <chen@rotemlevy.name>
2017-06-06 19:26:35 +0300

    Fix (most of) test_i18n

--- 20170613-mock/code/guessing-ee/tests/test_i18n.py
+++ 20170613-mock/code/guessing-ee/tests/test_i18n.py
@@ -8,35 +8,32 @@ except ImportError:
 from guessing import i18n
 
 
-def test_is_lang_default():
+def test_lang_default():
     with mock.patch.dict('guessing.i18n.environ', clear=True):
-        assert i18n.is_lang('C')
+        assert i18n.lang() == 'C'
 
-def test_is_lang_default_not_english():
-    with mock.patch.dict('guessing.i18n.environ', clear=True):
-        assert not i18n.is_lang('en')
-
-def test_is_lang_not_hebrew():
+def test_is_lang_en():
     with mock.patch.dict('guessing.i18n.environ',
                          dict(LC_ALL='en_US',
                               LC_MESSAGES='en_US',
                               LANG='en_US')):
-        assert not i18n.is_lang('he')
+        assert i18n.lang() == 'en'
 
-def test_is_lang_hebrew():
+def test_lang_lc_message_he():
     with mock.patch.dict('guessing.i18n.environ',
                          LC_MESSAGES='he_IL'):
         assert i18n.is_lang('he')
 
 
-@mock.patch('guessing.i18n.is_lang')
-def test_T_not_hebrew(mock_is_lang):
-    mock_is_lang.return_value = False
+@mock.patch('guessing.i18n.lang')
+def test_T_not_hebrew(mock_lang):
+    mock_lang.return_value = 'C'
     assert i18n.T('hello') == 'hello'
 
-@mock.patch('guessing.i18n.is_lang')
-def test_T_hebrew(mock_is_lang):
-    mock_is_lang.side_effect = lambda lang: lang == 'he'
+@mock.patch('guessing.i18n.CLIENT')
+@mock.patch('guessing.i18n.lang')
+def test_T_hebrew(mock_is_lang, mock_CLIENT):
+    mock_lang.side_effect = lambda lang: lang == 'he'
     assert i18n.T('hello') == 'OLLEH'
 
 
