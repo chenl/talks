238b0656e5011d223c3d8b190084d6ccee16676c
eliadl <eliadl@sentinelone.com>
2017-06-08 10:35:09 +0300

    Test guessing.play

--- 20170613-mock/code/guessing-ee/tests/test_gessing.py
+++ 20170613-mock/code/guessing-ee/tests/test_gessing.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from io import BytesIO
 try:
     from io import StringIO
 except ImportError:
@@ -32,6 +33,7 @@ def test_game(fun_times):
         assert game.play.call_count == 1 + fun_times
         game.outro.assert_called_once()
 
+
 @mock.patch('guessing.game.T', side_effect=lambda x: x)
 @mock.patch('random.seed')
 @mock.patch('sys.stdout' , new_callable=StringIO)
@@ -42,15 +44,53 @@ def test_intro(mock_stdout, mock_seed, mock_T):
     mock_seed.assert_called_once_with()
     assert mock_stdout.getvalue() == "{}\n".format(msg)
 
+
 @mock.patch('guessing.game.T', side_effect=lambda x: x)
-@mock.patch('sys.stdout' , new_callable=StringIO)
+@mock.patch('sys.stdout', new_callable=StringIO)
 def test_outro(mock_stdout, mock_T):
     game.outro()
     msg = "That was fun! Thank you for playing with me."
     mock_T.assert_called_once_with(msg)
     assert mock_stdout.getvalue() == "{}\n".format(msg)
 
+
 def test_max_number():
     assert game.max_number(1) == 1
     assert game.max_number(2) == 3
     assert game.max_number(3) == 7
+
+
+@pytest.mark.parametrize('guesses,expected_messages', [
+    ([4, 6, 5], [  # full scenario
+        "Level 3: 0 to 7",
+        "Can you guess what number I am thinking about?",
+        "No, my number is bigger than that",
+        "Can you guess what number I am thinking about?",
+        "No, my number is smaller than that",
+        "Can you guess what number I am thinking about?",
+        "Yes, this is the number I was thinking about! How did you know that?",
+    ]),
+    ([None], [     # empty scenario
+        "Level 3: 0 to 7",
+        "Can you guess what number I am thinking about?",
+        "Just wanted you to know that I was thinking about 5",
+    ]),
+    ([5], [        # lucky scenario
+        "Level 3: 0 to 7",
+        "Can you guess what number I am thinking about?",
+        "Yes, this is the number I was thinking about! How did you know that?",
+    ]),
+])
+@mock.patch('guessing.game.T', side_effect=lambda x: x)
+@mock.patch('sys.stdout', new_callable=BytesIO)
+def test_play(mock_stdout, mock_T, guesses, expected_messages):
+    level = 3
+    num = 5
+
+    with \
+            mock.patch('guessing.game.think_of_a_number', return_value=num), \
+            mock.patch('guessing.game.your_guess', side_effect=guesses):
+        game.play(level)
+
+    mock_T.call_args_list = map(mock.call, expected_messages)
+    assert mock_stdout.getvalue() == '\n'.join(expected_messages) + '\n'
